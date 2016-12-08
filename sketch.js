f3c2e67cc2c2470ef1a3fbe9f7db7b75b43c09a1
jandornig <jandornig@gmail.com>
2016-12-08 13:58:27 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,60 +1,62 @@
 var c = 0
-var s = 0
 var range = 50
-var colorR = []
-var colorG = []
-var colorB = []
+var lastTime= 0
+var updateEvery= 200
+var currentTime = 0
+var x = 0
+var y = 0
 
 function setup() {
 createCanvas(800,600)
 ellipseMode(CENTER)
-frameRate(5)
+frameRate(30)
 }
 
 function draw() {
   
-  colorR.push(random(256))
-  colorG.push(random(256))
-  colorB.push(random(256))
+  currentTime = millis()
   
   if (mouseIsPressed) {
     background(255)
   }
   
-  for(x = 30; x<windowWidth-30; x+=30) {
-    for (y= 30; y<windowHeight-30; y+=30){
-      
-      if(mouseX-pmouseX<30 && mouseY-pmouseY<30){
-        
-          range=range+0.01}
-          else{
-            range=50
-          }
-      
-      if(mouseX>x-range && mouseX<x+range && mouseY>y-range && mouseY<y+range){
-        
-        stroke(colorR[colorR.length-1],colorG[colorG.length-1],colorB[colorB.length-1])
-        tile()
-        
-        if(random()<0.5){
-          noStroke()
-          fill(255,50)
-          ellipse(x,y,30,30)
-        }
-      }
-      
-      else{
-        noStroke()
-        fill(255,50)
-        ellipse(x,y,30,30)
-      }
-  
-    
-  }
+  if(currentTime-lastTime>updateEvery){
+    createTexture(x,y)
+    lastTime=millis()
   }
 }
 
-function tile(){
+function createTexture(){
+  stroke(random(256),random(256),random(256))
+  
+  
+  
+  for(x = 30; x<windowWidth-30; x+=30) {
+    for (y= 30; y<windowHeight-30; y+=30){
+          
+      if(mouseX>x-range && mouseX<x+range && mouseY>y-range && mouseY<y+range){
+            
+      tile(x,y)
+      }
+            
+      /*else{
+          noStroke()
+          fill(255,50)
+          ellipse(x,y,30,30)
+      }*/
+      
+      if(mouseX-pmouseX<30 && mouseY-pmouseY<30){
+          range=range+0.01
+        }
+      else{
+        range=50
+      }
+      
+    }
+  }
+}
+
+function tile(x,y){
   noFill()
   if(random()<0.5){
   ellipse(x-5,y,random(10))
@@ -70,11 +72,5 @@ function tile(){
   }
   if(c<20 && random()<0.5){
   line(x-10,y-10,x+10,y+10)
-
   }
-}
-
-function click(){
-  
-  
 }
